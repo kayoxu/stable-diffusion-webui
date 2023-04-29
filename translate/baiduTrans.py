37424b7be34c25c6d52b849e7751bc5c1dadbce5
KayoXu <371714752@qq.com>
2023-04-29 11:39:28 +0800

    修改

--- translate/baiduTrans.py
+++ translate/baiduTrans.py
@@ -16,6 +16,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 def zh2en(data: str):
     if data == '' or data.lower().__contains__('@nts'):
+        data = data.replace('@NTS', '').replace('@nts', '')
         return data
 
     isok = True
